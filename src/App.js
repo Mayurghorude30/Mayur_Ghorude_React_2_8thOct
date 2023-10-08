f8bdf295debe60816e60325e6512a3e7700dcdcf
mayur ghorude <mayurghorude30@gmail.com>
2023-10-08 14:56:46 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CryptoData from './CryptoData'; 
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoData /> 
+    </div>
+  );
+}
+
+export default App;
